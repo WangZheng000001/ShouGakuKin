0b7b421b196b5b853cb92f85032e0f18a2232ee1
jiayi <kayi4862@gmail.com}>
2022-08-18 18:53:00 +0900

    增加登录,注册界面

--- ShouGakuKinSystem/src/main/java/com/example/demo/controller/HelloController.java
+++ ShouGakuKinSystem/src/main/java/com/example/demo/controller/HelloController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         //调用业务,接受前端的参数
         return "hello,world";
     }
@@ -24,13 +24,38 @@ public class HelloController {
     //HttpServletRequest对象代表客户端的请求，当客户端通过HTTP协议访问服务器时，HTTP请求头中的所有信息都封装在这个对象中，开发人员通过这个对象的方法，可以获得客户这些信息。
     public String loginCheck(HttpServletRequest request) {
         System.out.println("如下");
-        String id=request.getParameter("studentID");
-        String password=request.getParameter("passWord");
-        System.out.println("studentID:"+id);
-        System.out.println("passWord:"+password);
+        String id = request.getParameter("studentID");
+        String password = request.getParameter("passWord");
+        System.out.println("studentID:" + id);
+        System.out.println("passWord:" + password);
+
 
         return "redirect:NewFile.html";
 
-
     }
+
+    @PostMapping("/Registered")
+    public String Registered(HttpServletRequest request) {
+        System.out.println("学生登録手続き開始");
+        String id = request.getParameter("ID");
+        String name = request.getParameter("name");
+        String school = request.getParameter("school");
+        String grade = request.getParameter("grade");
+        String specialty = request.getParameter("specialty");
+        String gender = request.getParameter("gender");
+        String age = request.getParameter("age");
+        System.out.println("ID:" + id);
+        System.out.println("Name:" + name);
+        System.out.println("School:" + school);
+        System.out.println("Grade:" + grade);
+        System.out.println("Specialty:" + specialty);
+        System.out.println("Grender:" + gender);
+        System.out.println("Age:" + age);
+
+
+        return "redirect:/hello";
+    }
+
+
+
 }
